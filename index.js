de6207c22a1845058fab96a055ef1600595add29
Mikhail Gordeyev <m.gordeyev@gmail.com>
2019-09-13 18:30:23 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-class TemplateClass {
-    constructor() {
-
-    }
-
-    someMethod() {
-        return 'some return';
-    }
-}
-
-var freeGlobal = (typeof window !== 'undefined' ? window : (typeof self !== 'undefined' ? self : {})); // jshint ignore:line
-freeGlobal.Hammer = TemplateClass;
-
-if (typeof define === 'function' && define.amd) {
-    define(function() {
-        return TemplateClass;
-    });
-} else if (typeof module != 'undefined' && module.exports) {
-    module.exports = TemplateClass;
-} else {
-    window[exportName] = TemplateClass;
+exports.TemplateClass = function() {
+    console.log("This is a message from the demo package");
+    this.vasya = 'test';
 }
